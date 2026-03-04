92c7bf380733692b67723bc97a45c5b3f134a986
jp055ung <jpgames055@gmail.com>
2026-03-04 15:50:19 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: { outDir: 'dist/client', rollupOptions: { input: path.resolve(__dirname, 'index.html') } },
-  server: { proxy: { '/api': 'http://localhost:3000' } },
+  build: {
+    outDir: 'dist/client',
+  },
+  server: {
+    proxy: { '/api': 'http://localhost:3000' },
+  },
 });
